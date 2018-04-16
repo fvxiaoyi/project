c63f51bb6bd3f885b8a26fcf87195cae7ea419df
syb <syb@gixon.com>
2018-04-16 18:32:04 +0800

    -

--- pages/project/index/index.js
+++ pages/project/index/index.js
@@ -7,8 +7,8 @@ Page({
       name: '任务名字2',
       picture: 'https://mailimg.teambition.com/logos/cover-media.jpg'
     }, {
-        name: '任务名字3',
-        picture: 'https://mailimg.teambition.com/logos/cover-media.jpg'
+      name: '任务名字3',
+      picture: 'https://mailimg.teambition.com/logos/cover-media.jpg'
     }]
   }
 })
